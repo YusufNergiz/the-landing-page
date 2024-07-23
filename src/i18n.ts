da5e40cc01dd351be5731c757645119360bfbd83
Yussuf Nergiz <yussufnergiz@Yussufs-MacBook-Pro.local>
2024-07-23 21:23:54 +0200

    Revert "refactroed some code"
    This reverts commit cb47c455ceaf356264aa3b556612197a216fe6d2.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,14 @@
-import { notFound } from "next/navigation";
-import { getRequestConfig } from "next-intl/server";
-import { type AbstractIntlMessages } from "next-intl";
-import { locales, type Locale } from "./locales";
+import {notFound} from 'next/navigation';
+import {getRequestConfig} from 'next-intl/server';
 
-const messageImports = {
-    en: () => import("../messages/en.json"),
-    pl: () => import("../messages/pl.json"),
-} as const satisfies Record<Locale, () => Promise<{ default: AbstractIntlMessages }>>;
+// Can be imported from a shared config
+const locales = ['en', 'pl'];
 
-export function isValidLocale(locale: unknown): locale is Locale {
-    return locales.some((l) => l === locale);
-}
+export default getRequestConfig(async ({locale}) => {
+    // Validate that the incoming `locale` parameter is valid
+    if (!locales.includes(locale as any)) notFound();
 
-export default getRequestConfig(async (params) => {
-    const baseLocale = new Intl.Locale(params.locale).baseName;
-    if (!isValidLocale(baseLocale)) notFound();
-
-    const messages = (await messageImports[baseLocale]()).default;
     return {
-        messages,
+        messages: (await import(`../messages/${locale}.json`)).default
     };
 });
